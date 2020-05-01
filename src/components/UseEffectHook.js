4fb7b1415d15beb2d3b2f0c83ec923b5b5bd93ab
hamzakhalil3343 <hamza.khalil3343@gmail.com>
2020-05-02 00:04:44 +0500

    useEffect Hook with Conditionally run effects

--- src/components/UseEffectHook.js
+++ src/components/UseEffectHook.js
@@ -2,15 +2,18 @@ import React,{useState,useEffect} from 'react';
 
 function UseEffectHook(props) {
     const [count,setcount]=useState(0);
+    const [name,setName]=useState('');
     useEffect(
-        ()=>{
-    document.title=`you click ${count} times`;
-}
+                ()=>{
+                document.title=`you click ${count} times`;
+                console.log('updating count times');
+                  }  ,[count]                                      //it will check the condition simply if updating count or Any Thing  else 
 
-    )
+
+)
     return (
         <div>
-            hamza
+            <input type="text" value={name} onChange={e=>setName(e.target.value)} />
             <button onClick={()=>setcount(count+1)}>click {count}</button>
             
         </div>
@@ -18,3 +21,5 @@ function UseEffectHook(props) {
 }
 
 export default UseEffectHook;
+
+//useEffect is called after Every Single Render
